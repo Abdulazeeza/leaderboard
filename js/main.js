a2f0a161e5dd24d9451f1cfbe025f39778bda262
unknown <abdulazeeza180@gmail.com>
2020-06-22 22:50:19 +0100

    feat:implemented leaderboard for HNG

---
+++ js/main.js
@@ -0,0 +1,719 @@
+data = [
+	{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 100,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 500,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 90,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 150,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 15,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 15,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+	{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 15,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 15,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+	{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 15,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 15,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },{
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  },
+  {
+    full_name: "Abdulazeez Abdulazeez",
+    user_name: "AbdulazeezAA",
+    email: "abdulazeeza180@gmail.com",
+    point: 10,
+  }
+];
+
+let present_num = 0;
+let previous_num = 0;
+const create_table =(table_data) =>{
+	$('.table_body').html('');
+	const table = document.createElement('table');
+	table.className = 'leader_table';
+	table_header = document.createElement('tr');
+	table_header.innerHTML = `<th class="candidate_SN_title">S/N</th>
+					<th class="candidate_fullname_title">Full Name</th>
+			        <th class="candidate_username_title">UserName</th>
+			        <th class="candidate_email_title">Email</th>
+			        <th class="present_point_title">Point</th>`;
+	$(table).append(table_header);
+	$('.table_body').append(table);
+
+	for(num = present_num; num <= table_data.length; num++){
+		present_num = num + 1;
+        previous_num = num + 1
+		table_row = document.createElement('tr');
+		if(num < 3){
+			table_row.innerHTML = `<td class="candidate_SN" style="background:#FFFFE0;">${present_num}</td>
+	    	<td class="candidate_fullname" style="background:#FFFFE0;">${table_data[num].full_name}</td>
+	        <td class="candidate_username" style="background:#FFFFE0;">${table_data[num].user_name}</td>
+	        <td class="candidate_email" style="background:#FFFFE0;">${table_data[num].email}</td>
+	        <td class="present_point" style="background:#FFFFE0;">${table_data[num].point}<a class="twitter-share-button"
+			href="https://twitter.com/intent/tweet?text=My%20Present%20Point%20On%20%20HNG%20Internship%20is%20${table_data[num].point}">
+			tweet</a></td>`;
+		}
+		else{
+			table_row.innerHTML = `<td class="candidate_SN">${present_num}</td>
+	    	<td class="candidate_fullname">${table_data[num].full_name}</td>
+	        <td class="candidate_username">${table_data[num].user_name}</td>
+	        <td class="candidate_email">${table_data[num].email}</td>
+	        <td class="present_point">${table_data[num].point}<a class="twitter-share-button"
+			href="https://twitter.com/intent/tweet?text=My%20Present%20Point%20On%20%20HNG%20Internship%20is%20${table_data[num].point}">
+			tweet</a></td>`;
+		}
+        $('table').append(table_row);
+        if(present_num % 40 == 0 || present_num == data.length){
+        	break;
+        }
+	}
+
+}
+const arrange_num_ascending =(a, b)=>{
+	// a should come before b in the sorted order
+    if(a.point < b.point){
+            return -1;
+    // a should come after b in the sorted order
+    }else if(a.point > b.point){
+            return 1;
+    // a and b are the same
+    }else{
+            return 0;
+    }
+}
+const arrange_num_decending =(a, b)=>{
+    if(a.point < b.point){
+            return 1;
+    }else if(a.point > b.point){
+            return -1;
+    }else{
+            return 0;
+    }
+}
+$(document).on("click", ".prev_table", ()=>{
+	
+	if(present_num >= previous_num){
+		present_num = present_num - previous_num;
+		create_table(data);
+		window.scrollTo(0, 0); 
+	}
+});
+$(document).on("click", ".next_table", ()=>{
+	if(present_num < data.length && present_num != data.length){
+		create_table(data);
+		previous_num = previous_num - 40;
+		window.scrollTo(0, 0); 
+	}
+});
+$(document).on("click", ".sort_by_lowest", ()=>{
+	data.sort(arrange_num_ascending);
+	present_num = present_num - 40;
+	create_table(data);
+});
+$(document).on("click", ".sort_by_highest", ()=>{
+	data.sort(arrange_num_decending);
+	present_num = present_num - 40;
+	create_table(data);
+});
+create_table(data);
+$('.sort_by_highest').click();
